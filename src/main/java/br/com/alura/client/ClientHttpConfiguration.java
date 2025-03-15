0d1f1ddbeb5b7f6ab1fab923b6eb4bfe404aebae
devlucasalmd <lucas.amaral.almd@gmail.com>
2025-03-14 23:17:38 -0300

    feat(Service e domain): Criando Services e objetos

--- src/main/java/br/com/alura/client/ClientHttpConfiguration.java
+++ src/main/java/br/com/alura/client/ClientHttpConfiguration.java
@@ -6,7 +6,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
+import br.com.alura.domain.Abrigo;
 
 public class ClientHttpConfiguration {
 	
@@ -20,12 +23,12 @@ public class ClientHttpConfiguration {
     	return client.send(request, HttpResponse.BodyHandlers.ofString());    	
 	}
     
-    public static HttpResponse<String> dispararRequisicaoPost(String uri, JsonObject json) throws IOException, InterruptedException {
+    public static HttpResponse<String> dispararRequisicaoPost(String uri, Object object) throws IOException, InterruptedException {
     	HttpClient client = HttpClient.newHttpClient(); 
     	HttpRequest request = HttpRequest.newBuilder()
                  .uri(URI.create(uri))
                  .header("Content-Type", "application/json")
-                 .method("POST", HttpRequest.BodyPublishers.ofString(json.toString()))
+                 .method("POST", HttpRequest.BodyPublishers.ofString(new Gson().toJson(object)))
                  .build();
 
          return client.send(request, HttpResponse.BodyHandlers.ofString());
